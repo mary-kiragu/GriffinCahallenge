60053779312bea9e78eeb96051c379493bb5fa06
mary-kiragu <marywanjiku.mw18@gmail.com>
2021-01-20 20:42:57 +0300

    second commit

--- script.js
+++ script.js
@@ -1,19 +1,67 @@
-if (document.readyState == 'loading') {
-    document.addEventListener('DOMContentLoaded', loaded)
-}else{
-    loaded();
+const cartBtn = document.querySelector(".cartButton");
+
+const closeCartBtn = document.querySelector(".close-cart");
+
+const clearCartBtn = document.querySelector(".clearCart");
+
+const cartDiv = document.querySelector(".cart");
+
+const cartOverlay = document.querySelector(".cart-overlay");
+
+const cartItem = document.querySelector(".cartItems");
+
+const cartTotal = document.querySelector(".cartTotal");
+
+const cartContent = document.querySelector(".cart-content");
+
+const fruitsCenter = document.querySelector(".fruits-center");
+
+let cart = [];
+//used to get cart items
+class fruits{
+async getFruits(){
+try {
+    let result = await fetch('NewGriffinChallenge.json');
+    let data = await result.json();
+   return data; 
+} catch (error) {
+  console.log(Error) ; 
+}
+    
+}
+}
+class fruitDisplay{
+
+}
+class fruitStore{
+
 }
 
-function loaded(){
-    var button = document.getElementsByClassName('add-to-cart-btn');
-    button.addEventListener('click', addToCart());
+document.addEventListener("DOMContentLoaded",()=>{
 
-    console.log(apple);
-}
-function addToCart(){
-    var apple = document.getElementsByClassName('apple')[0].innerText;
-    var appleImage = document.getElementsByClassName('fruit-image')[0].src;
-    var applePrice = document.getElementsByClassName('apple-price')[0].innerText;
-    console.log(applePrice);
+    // const fruitDisplay = new fruitDisplay();
+     const fruits = new fruits();
+    //getting all fruits stored
+    
+    console.log(fruits.getFruits());
+})
 
-}
+// if (document.readyState == 'loading') {
+//     document.addEventListener('DOMContentLoaded', loaded)
+// }else{
+//     loaded();
+// }
+
+// function loaded(){
+//     var button = document.getElementsByClassName('add-to-cart-btn');
+//     button.addEventListener('click', addToCart());
+
+//     console.log(apple);
+// }
+// function addToCart(){
+//     var apple = document.getElementsByClassName('apple')[0].innerText;
+//     var appleImage = document.getElementsByClassName('fruit-image')[0].src;
+//     var applePrice = document.getElementsByClassName('apple-price')[0].innerText;
+//     console.log(applePrice);
+
+// }
